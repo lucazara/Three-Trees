7718661607d71fd5f9e02fdc53f8325529a07857
lucazara <lucazara33@gmail.com>
2022-10-27 20:00:57 +0200

    struttura base barre superiore e inferiore, player prefs,  font pixelato

--- ThreeTrees/Assets/Scripts/LymphScript.cs
+++ ThreeTrees/Assets/Scripts/LymphScript.cs
@@ -6,31 +6,44 @@ using UnityEngine.UI;
 public class LymphScript : MonoBehaviour
 {
 
-    private int lymph;
+    private int lymph = 0;
     private int lymph_per_click;
 
     private Text lymph_text;
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        lymph = 0;
-        lymph_per_click = 1;
         lymph_text = GetComponent<Text>();
+
+        lymph = PlayerPrefs.GetInt("lymph", 0);
+        lymph_per_click = PlayerPrefs.GetInt("lymph_per_click", 1);
+
+        lymph_text.text = lymph.ToString() + " lymph";
     }
 
-    // Update is called once per frame
     void Update()
     {
         foreach (Touch touch in Input.touches)
         {
             if (touch.phase == TouchPhase.Began)
             {
-                lymph += lymph_per_click;
-                lymph_text.text = lymph.ToString();
+                produceLymph();
+                lymph_text.text = lymph.ToString() + " lymph";
             }
         }
+
+        if (Input.GetKeyDown("c"))
+        {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("player prefs cleared");
+        }
+    }
+
+
+    void produceLymph()
+    {
+        lymph += lymph_per_click;
+        PlayerPrefs.SetInt("lymph", lymph);
     }
 }

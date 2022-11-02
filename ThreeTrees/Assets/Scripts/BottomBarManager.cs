1b6e9a13b44b4dfd84d7df97d6bf0b43a1905089
lucazara <lucazara33@gmail.com>
2022-11-02 15:02:04 +0100

    schermata base upgrades

--- ThreeTrees/Assets/Scripts/BottomBarManager.cs
+++ ThreeTrees/Assets/Scripts/BottomBarManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class BottomBarManager : MonoBehaviour
 {
     public int screenIndex;
+
     public GameObject home;
+    public GameObject upgrades;
 
     void Start()
     {
@@ -19,30 +21,34 @@ public class BottomBarManager : MonoBehaviour
     {
         screenIndex = 0;
         home.SetActive(false);
+        upgrades.SetActive(false);
     }
 
     public void ToForest()
     {
         screenIndex = 1;
         home.SetActive(false);
+        upgrades.SetActive(false);
     }
 
     public void ToHome()
     {
         screenIndex = 2;
         home.SetActive(true);
-
+        upgrades.SetActive(false);
     }
 
     public void ToUpgrades()
     {
         screenIndex = 3;
         home.SetActive(false);
+        upgrades.SetActive(true);
     }
 
     public void ToShop()
     {
         screenIndex = 4;
         home.SetActive(false);
+        upgrades.SetActive(false); 
     }
 }
